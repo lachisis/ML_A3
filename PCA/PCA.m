b295a253a835d001692f0b47282781fd983a6880
lachisis <se_raimondo@rogers.com>
2014-11-29 00:19:57 -0500

    PCA fix
    PCA basic: 59.32%
    PCA (less first 4 dims): 59.41%

--- PCA/PCA.m
+++ PCA/PCA.m
@@ -21,14 +21,21 @@ X = double(collapse_image_matrix(tr_images));
 [base,ed,mean,projX] = pcaimg(X,m);
 
 %%
-visualize_digits(base*8);
+%visualize_digits(base*8);
 
 %% NOW CLASSIFY
-for m = [10 20 30 40 50 100 200 300 400 500]
+m_list = [1 2 4 6 8 10 20 30 40 50 100 200];
+acc_vs_m = zeros(1,length(m_list));
+k_val = zeros(1,length(m_list));
+for i = 1:length(m_list)
+    m = m_list(i);
     fprintf('PCA dim m: %d\n', m);
     X = double(collapse_image_matrix(tr_images));
     [base,ed,mean,projX] = pcaimg(X,m);    
     
+    visualize_digits(base*10);
+    title_string = strcat('PCA dims: ',int2str(m));
+    title(title_string)
     train_set = projX;
     nfold = 10;
     
@@ -39,6 +46,94 @@ for m = [10 20 30 40 50 100 200 300 400 500]
     end
     [maxacc bestK] = max(acc);
     fprintf('K is selected to be %d.\n', bestK);
+    
+    k_val(i) = bestK;
+    acc_vs_m(i) = maxacc;
 end
 
+%%
+figure
+[ax,p1,p2] = plotyy(m_list, k_val,  m_list, acc_vs_m);
+ylabel(ax(1),'Best K') % label left y-axis
+ylabel(ax(2),'Accuracy') % label right y-axis
+xlabel(ax(2),'m') % label x-axis
+set(p1,'Marker', 'o')
+set(p2,'Marker', 'o')
 
+
+%% NOW CLASSIFY
+m_list = 35:45;
+acc_vs_m = zeros(1,length(m_list));
+k_val = zeros(1,length(m_list));
+for i = 1:length(m_list)
+    m = m_list(i);
+    fprintf('PCA dim m: %d\n', m);
+    X = double(collapse_image_matrix(tr_images));
+    [base,ed,mean,projX] = pcaimg(X,m);    
+    
+    visualize_digits(base*10);
+    title_string = strcat('PCA dims: ',int2str(m));
+    title(title_string)
+    train_set = projX;
+    nfold = 10;
+    
+    for K=[1:10 15 20 35 50]
+      nfold = 10;
+      acc(K) = cross_validate_for_PCA(K, train_set, tr_labels, nfold, tr_identity);
+      fprintf('%d-fold cross-validation with K=%d resulted in %.4f accuracy\n', nfold, K, acc(K));
+    end
+    [maxacc bestK] = max(acc);
+    fprintf('K is selected to be %d.\n', bestK);
+    
+    k_val(i) = bestK;
+    acc_vs_m(i) = maxacc;
+end
+
+%%
+figure
+[ax,p1,p2] = plotyy(m_list, k_val,  m_list, acc_vs_m);
+ylabel(ax(1),'Best K') % label left y-axis
+ylabel(ax(2),'Accuracy') % label right y-axis
+xlabel(ax(2),'m') % label x-axis
+set(p1,'Marker', 'o')
+set(p2,'Marker', 'o')
+
+%% NOW CLASSIFY
+m_list = [5 10 20 30 40 50 60];
+acc_vs_m = zeros(1,length(m_list));
+k_val = zeros(1,length(m_list));
+for i = 1:length(m_list)
+    m = m_list(i);
+    fprintf('PCA dim m: %d\n', m);
+    X = double(collapse_image_matrix(tr_images));
+    [base,ed,mean,projX] = pcaimg(X,m);    
+    %kick out the first couple of dimensions
+    %base = base(:,3:end);
+    base = base(:,5:end);
+    
+    visualize_digits(base*10);
+    title_string = strcat('PCA dims: ',int2str(m));
+    title(title_string)
+    train_set = projX;
+    nfold = 10;
+    
+    for K=[1:10 15 20 35 50]
+      nfold = 10;
+      acc(K) = cross_validate_for_PCA(K, train_set, tr_labels, nfold, tr_identity);
+      fprintf('%d-fold cross-validation with K=%d resulted in %.4f accuracy\n', nfold, K, acc(K));
+    end
+    [maxacc bestK] = max(acc);
+    fprintf('K is selected to be %d.\n', bestK);
+    
+    k_val(i) = bestK;
+    acc_vs_m(i) = maxacc;
+end
+
+%%
+figure
+[ax,p1,p2] = plotyy(m_list, k_val,  m_list, acc_vs_m);
+ylabel(ax(1),'Best K') % label left y-axis
+ylabel(ax(2),'Accuracy') % label right y-axis
+xlabel(ax(2),'m') % label x-axis
+set(p1,'Marker', 'o')
+set(p2,'Marker', 'o')
--- PCA/knn_classifier_for_PCA.m
+++ PCA/knn_classifier_for_PCA.m
@@ -2,29 +2,29 @@
 function [test_prediction] = knn_classifier_for_PCA(KNN, tr_images, tr_labels, test_images)
 
 % Reshape into vectors
-ntr = size(tr_images, 3);
-ntest = size(test_images, 3);
-% h = size(tr_images,1);
-% w = size(tr_images,2);
-% tr_images = double(reshape(tr_images, [h*w, ntr]));
-% test_images = double(reshape(test_images, [h*w, ntest]));
+ntr = size(tr_images, 2);
+ntest = size(test_images, 2);
+%h = size(tr_images,1);
+%w = size(tr_images,2);
+%tr_images = double(reshape(tr_images, [h*w, ntr]));
+%test_images = double(reshape(test_images, [h*w, ntest]));
 
-% % Subtract mean for each image
-% tr_mu = mean(tr_images);
-% test_mu = mean(test_images);
-% tr_images = bsxfun(@minus, tr_images, tr_mu);
-% test_images = bsxfun(@minus, test_images, test_mu);
-% 
-% % Normalize variance for each image
-% tr_sd = var(tr_images);
-% tr_sd = tr_sd + 0.01; % for extreme cases
-% tr_sd = sqrt(tr_sd);
-% tr_images = bsxfun(@rdivide, tr_images, tr_sd);  
-% 
-% test_sd = var(test_images);
-% test_sd = test_sd + 0.01; % for extreme cases
-% test_sd = sqrt(test_sd);
-% test_images = bsxfun(@rdivide, test_images, test_sd);  
+% Subtract mean for each image
+tr_mu = mean(tr_images);
+test_mu = mean(test_images);
+tr_images = bsxfun(@minus, tr_images, tr_mu);
+test_images = bsxfun(@minus, test_images, test_mu);
+
+% Normalize variance for each image
+tr_sd = var(tr_images);
+tr_sd = tr_sd + 0.01; % for extreme cases
+tr_sd = sqrt(tr_sd);
+tr_images = bsxfun(@rdivide, tr_images, tr_sd);  
+
+test_sd = var(test_images);
+test_sd = test_sd + 0.01; % for extreme cases
+test_sd = sqrt(test_sd);
+test_images = bsxfun(@rdivide, test_images, test_sd);  
 
 test_prediction = zeros(ntest, 1);
 
