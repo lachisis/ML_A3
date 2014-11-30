9e8164c8995901c6eba7f9a8ec1cbaaa48eadc8b
lachisis <se_raimondo@rogers.com>
2014-11-30 18:58:46 -0500

    Cross validation update

--- cross_validate_indeces_with_test_set.m
+++ cross_validate_indeces_with_test_set.m
@@ -2,7 +2,7 @@
 % If tr_identity is provided, uses that to do the split of the training images
 % If tr_identity is not provided uses random permutations (disregards similar faces, bias in the training data)
 
-function [cross_val_train_inds, cross_val_valid_inds] = cross_validate_indeces(tr_images, nfold, tr_identity)
+function [cross_val_train_ids, cross_val_valid_ids, test_ids] = cross_validate_indeces_with_test_set(tr_images, nfold, tr_identity)
 
 ntr = size(tr_images, 2);
 
@@ -10,11 +10,14 @@ if (~exist('tr_identity', 'var'))
   % random permutation (disregards similar faces)
   perm = randperm(ntr); 
 
-  foldsize = floor(ntr/nfold);
+  testsize = floor(ntr/(nfold + 1));
+  ntronly = ntr - testsize;
+  foldsize = floor(ntronly/nfold);
   for i=1:nfold-1
     foldids{i} = (i-1)*foldsize+1:(i*foldsize);
   end
-  foldids{nfold} = (nfold-1)*foldsize+1:ntr;
+  foldids{nfold} = ((nfold-1)*foldsize+1):ntronly;
+  test_ids{1} = ntronly + 1:ntr;
 else
   % generally one uses random permutation to specify the splits, but because of the special structure of the dataset
   % we use the identity of poeple for this purpose.
@@ -37,22 +40,25 @@ else
   people = people(randperm(npeople));
   
   % dividing people into groups of roughly the same size but not necessarily
-  foldsize = floor(npeople/nfold);
+  testsize = floor(npeople/(nfold + 1));
+  ntronly = npeople - testsize;
+  foldsize = floor(ntronly/nfold);
   for i=1:nfold-1
     foldids{i} = [people{(i-1)*foldsize+1:(i*foldsize)}];
   end
-  foldids{nfold} = [people{(nfold-1)*foldsize+1:npeople}];
+  foldids{nfold} = [people{(nfold-1)*foldsize+1:ntronly}];
+  test_ids{1} = [people{ntronly + 1:npeople}];
 end
 
 % perform nfold training and validation
-cross_val_train_inds = cell(1,nfold);
-cross_val_valid_inds = cell(1,nfold);
+cross_val_train_ids = cell(1,nfold);
+cross_val_valid_ids = cell(1,nfold);
 for i=1:nfold
   traini_ids = [foldids{[1:(i-1) (i+1):nfold]}];
   testi_ids = foldids{i};
   
-  cross_val_train_inds{i} = traini_ids;
-  cross_val_valid_inds{i} = testi_ids;
+  cross_val_train_ids{i} = traini_ids;
+  cross_val_valid_ids{i} = testi_ids;
 end
 
 end
